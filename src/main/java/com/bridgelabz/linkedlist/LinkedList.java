34774036f22c8fb80e2f92cd84a9a358dc0c3471
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-31 23:08:54 +0530

    ADDED : Welcome To LinkedList Program

---
+++ src/main/java/com/bridgelabz/linkedlist/LinkedList.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        // Displaying Welcome Message
+        System.out.println("Welcome to LinkedList Program");
+    }
+}
