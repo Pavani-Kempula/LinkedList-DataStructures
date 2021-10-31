9be97063761e414ad96b102d2e1523cd6323b67d
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-01 00:00:29 +0530

    ADDED : Appending new node to the last position of the linkedlist to reach to the last position first using while loop then appending new node at last position

--- src/main/java/com/bridgelabz/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/bridgelabz/linkedlist/SinglyLinkedList.java
@@ -10,7 +10,7 @@ public class SinglyLinkedList {
     //Initially head is tail.
     Node head = null;
 
-    /*
+    /**
      * Name : addNode
      *
      * Description : Create or add New-Node.
@@ -22,21 +22,59 @@ public class SinglyLinkedList {
      * If head is null means there is no node present.
      * If head is not null means there is node present and new node will get added at first position,
      * and new node have reference to the old node.
+     *
+     * Modification : First commit 02-July-2021
      */
-    public void addNode(int data) {
+    public void addNode(int data)
+    {
         Node newNode = new Node(data); //empty node
 
-        if(head == null) {
+        if(head == null)
+        {
             head = newNode;
             newNode.nextNode = null;
         }
-        else {
+        else
+        {
             newNode.nextNode = head;
             head = newNode;
         }
     }
 
-    /*
+    /**
+     * Name : appendAtLast
+     *
+     * Description : Appending newly node at the last.
+     *
+     * @param data
+     *
+     * Algorithm : Traversing the linkedlist using while loop.
+     * when the nextNode is null comes appending the newly created node to it
+     * which becomes the last node of linkedlist.
+     *
+     * Modification : First commit 02-July-2021
+     */
+    public void appendAtLast(int data)
+    {
+        Node newNode = new Node(data);
+
+        if(head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node tempNode ;
+            tempNode = head;
+            while(tempNode.nextNode != null)
+            {
+                tempNode = tempNode.nextNode;
+            }
+            tempNode.nextNode = newNode;
+        }
+    }
+
+    /**
      * Name : printNode
      *
      * Description : Printing LinkedList
@@ -45,16 +83,21 @@ public class SinglyLinkedList {
      * until it comes to nextNode which is null.
      * Printing the current node as tempNode moves forward.
      *
+     * Modification : First commit 02-July-2021
      */
-    public void printNode() {
+    public void printNode()
+    {
         Node tempNode;
         tempNode = head;
 
-        if(head == null) {
+        if(head == null)
+        {
             System.out.println("LinkedList is empty !");
         }
-        else {
-            while(tempNode.nextNode != null) {
+        else
+        {
+            while(tempNode.nextNode != null)
+            {
                 System.out.print(tempNode.data + " --> ");
                 tempNode = tempNode.nextNode;
             }
